ec64412ecc23ca8994bf5ac8e4d158950c2796f9
Alexander Londoño Espejo <alexlondon07@gmail.com>
2018-02-11 11:54:19 -0500

    first commit

---
+++ app/src/main/java/io/github/alexlondon07/realm/BoarActivity.java
@@ -0,0 +1,13 @@
+package io.github.alexlondon07.realm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boar);
+    }
+}
