cdf7bee41b7d67deb2d6f454b6829e7d20d13869
Alexander Londoño Espejo <alexlondon07@gmail.com>
2018-02-11 19:32:49 -0500

    Listview

--- app/src/main/java/io/github/alexlondon07/realm/activities/BoardActivity.java
+++ app/src/main/java/io/github/alexlondon07/realm/activities/BoardActivity.java
@@ -1,15 +1,33 @@
 package io.github.alexlondon07.realm.activities;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import io.github.alexlondon07.realm.R;
 
 public class BoardActivity extends AppCompatActivity {
 
+    private FloatingActionButton floatingActionButton;
+    private ListView listView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boar);
+        loadView();
+    }
+
+    private void loadView() {
+        floatingActionButton = findViewById(R.id.floatActionButtonAdd);
+    }
+
+
+    private void showAlertForCreatingboard(String title, String message){
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
     }
 }
